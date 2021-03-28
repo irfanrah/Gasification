ae8c8e8c7855c65e653dc6a4a4e716e1dfc425b5
irfanrah <rahadikirfan@gmail.com>
2021-03-28 16:26:24 +0100

    asd

--- iot/FrontEnd/combine/serialtest.py
+++
@@ -1,7 +0,0 @@
-
-def ArduRead():
-     Serial = serial.Serial('/dev/ttyUSB0', 9600) # Establish the connection on a specific port
-     ArduinoIn = str(Serial.readline()) 
-     ArduinoCut = ArduinoIn[2:-5]
-     time.sleep(0.5) 
-     return ArduinoCut
